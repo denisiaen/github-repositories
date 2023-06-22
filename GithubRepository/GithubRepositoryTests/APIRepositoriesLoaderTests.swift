dd6ee655e5d96bc2e4d0ec15c2e54e310815b1f8
Denisia Enasescu <denisia.enasescu.dev@gmail.com>
2023-06-22 17:52:18 +0300

    `APIRepositoriesLoader` load requests data from URL.

--- GithubRepository/GithubRepositoryTests/APIRepositoriesLoaderTests.swift
+++ GithubRepository/GithubRepositoryTests/APIRepositoriesLoaderTests.swift
@@ -13,13 +13,15 @@ protocol HTTPClient {
 
 class APIRepositoriesLoader {
     private let client: HTTPClient
+    private let url: URL
     
-    init(client: HTTPClient) {
+    init(client: HTTPClient, url: URL) {
         self.client = client
+        self.url = url
     }
     
     func load() {
-        client.get(from: URL(string: "https://url")!)
+        client.get(from: url)
     }
 }
 
@@ -40,18 +42,19 @@ final class APIRepositoriesLoaderTests: XCTestCase {
     }
 
     func test_load_requestsDataFromURL() {
-        let (sut, client) = makeSUT()
+        let url = URL(string: "https://a-given-url")!
+        let (sut, client) = makeSUT(url: url)
 
         sut.load()
         
-        XCTAssertNotNil(client.requestedURL)
+        XCTAssertEqual(client.requestedURL, url)
     }
     
     // MARK: - Helpers
     
-    private func makeSUT() -> (sut: APIRepositoriesLoader, client: HTTPClientSpy) {
+    private func makeSUT(url: URL = URL(string: "https://a-url")!) -> (sut: APIRepositoriesLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
-        let sut = APIRepositoriesLoader(client: client)
+        let sut = APIRepositoriesLoader(client: client, url: url)
         return (sut, client)
     }
 }
