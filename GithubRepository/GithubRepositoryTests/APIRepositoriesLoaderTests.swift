a2dc845f2b4d45874e0d4548627084a857554feb
Denisia Enasescu <denisia.enasescu.dev@gmail.com>
2023-06-22 18:36:56 +0300

    Delivers invalidData error on 200 HTTP response with invalid json.

--- GithubRepository/GithubRepositoryTests/APIRepositoriesLoaderTests.swift
+++ GithubRepository/GithubRepositoryTests/APIRepositoriesLoaderTests.swift
@@ -23,14 +23,14 @@ class HTTPClientSpy: HTTPClient {
         messages[index].completion(.failure(error))
     }
     
-    func complete(withStatusCode code: Int, at index: Int = 0) {
+    func complete(withStatusCode code: Int, data: Data = Data(), at index: Int = 0) {
         let response = HTTPURLResponse(
             url: requestedURLs[index],
             statusCode: code,
             httpVersion: nil,
             headerFields: nil
         )!
-        messages[index].completion(.success(response))
+        messages[index].completion(.success(data, response))
     }
 }
 
@@ -83,6 +83,18 @@ final class APIRepositoriesLoaderTests: XCTestCase {
             client.complete(withStatusCode: code, at: index)
             XCTAssertEqual(capturedError, [.invalidData])
         }
+    }
+    
+    func test_load_deliversErrorOn200HTTPResponseWithInvalidJSON() {
+        let (sut, client) = makeSUT()
+        
+        var receivedError = [APIRepositoriesLoader.Error]()
+        sut.load { receivedError.append($0) }
+        
+        let invalidJSON = Data("invalid data".utf8)
+        client.complete(withStatusCode: 200, data: invalidJSON)
+
+        XCTAssertEqual(receivedError, [.invalidData])
     }
         
     // MARK: - Helpers
