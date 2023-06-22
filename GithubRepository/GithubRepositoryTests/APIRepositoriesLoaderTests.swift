5a7679ad3105701a3290f5628525d680dd3188ad
Denisia Enasescu <denisia.enasescu.dev@gmail.com>
2023-06-22 17:46:03 +0300

    Move test logic to a separate type `HTTPClientSpy`. Make the `HTTPClient` a protocol. Inject `HTTPClient` in `APIRepositoriesLoader` on creation.

--- GithubRepository/GithubRepositoryTests/APIRepositoriesLoaderTests.swift
+++ GithubRepository/GithubRepositoryTests/APIRepositoriesLoaderTests.swift
@@ -7,21 +7,51 @@
 
 import XCTest
 
-class HTTPClient {
-    var requestedURL: URL?
+protocol HTTPClient {
+    func get(from url: URL)
 }
 
 class APIRepositoriesLoader {
+    private let client: HTTPClient
     
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    func load() {
+        client.get(from: URL(string: "https://url")!)
+    }
+}
+
+class HTTPClientSpy: HTTPClient {
+    var requestedURL: URL?
+    
+    func get(from url: URL) {
+        requestedURL = url
+    }
 }
 
 final class APIRepositoriesLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
-        let _ = APIRepositoriesLoader()
+        let (_, client) = makeSUT()
         
         XCTAssertNil(client.requestedURL)
     }
 
+    func test_load_requestsDataFromURL() {
+        let (sut, client) = makeSUT()
+
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT() -> (sut: APIRepositoriesLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = APIRepositoriesLoader(client: client)
+        return (sut, client)
+    }
 }
