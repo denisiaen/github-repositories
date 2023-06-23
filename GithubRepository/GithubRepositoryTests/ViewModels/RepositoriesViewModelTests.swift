28a7b188a11b9cbc38ec681c7abddd733020ffb9
Denisia Enasescu <denisia.enasescu.dev@gmail.com>
2023-06-23 17:47:57 +0300

    Guarantee `RepositoryItem` list is empty upon view model creation.

--- GithubRepository/GithubRepositoryTests/ViewModels/RepositoriesViewModelTests.swift
+++ GithubRepository/GithubRepositoryTests/ViewModels/RepositoriesViewModelTests.swift
@@ -6,8 +6,30 @@
 //
 
 import XCTest
+import GithubRepository
+
+public class RepositoriesViewModel: ObservableObject {
+    
+    @Published public var repositoryItems = [RepositoryItem]()
+    @Published public var isLoading = false
+    @Published public var error: Error?
+}
 
 final class RepositoriesViewModelTests: XCTestCase {
-
     
+    func test_init_deliversEmptyList() {
+        let sut = makeSUT()
+        
+        XCTAssertTrue(sut.repositoryItems.isEmpty)
+        XCTAssertFalse(sut.isLoading)
+        XCTAssertNil(sut.error)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> RepositoriesViewModel {
+        let sut = RepositoriesViewModel()
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
 }
