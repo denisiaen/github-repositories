bdcda18287b55db3729caa303a39f137ccde7a98
Denisia Enasescu <denisia.enasescu.dev@gmail.com>
2023-06-23 17:32:35 +0300

    Group test files to folders.

---
+++ GithubRepository/GithubRepositoryTests/ViewModels/RepositoriesViewModelTests.swift
@@ -0,0 +1,13 @@
+//
+//  RepositoriesViewModelTests.swift
+//  GithubRepositoryTests
+//
+//  Created by Denisia Enasescu on 23.06.2023.
+//
+
+import XCTest
+
+final class RepositoriesViewModelTests: XCTestCase {
+
+    
+}
