a7f09d5e84bcb81b7091cb5f6ceb120d49c0de8a
Denisia Enasescu <denisia.enasescu.dev@gmail.com>
2023-06-23 15:56:14 +0300

    Creates `URLSessionProtocol` which exposes the method I need from` URLSession` framework. Creates URLSession data task with URL on `URLSessionHTTPClient.get(from: URL)`.

---
+++ GithubRepository/GithubRepositoryTests/URLSessionHTTPClientTests.swift
@@ -0,0 +1,89 @@
+//
+//  URLSessionHTTPClientTests.swift
+//  GithubRepositoryTests
+//
+//  Created by Denisia Enasescu on 23.06.2023.
+//
+
+import XCTest
+import GithubRepository
+
+public protocol URLSessionProtocol {
+    func data(for request: URLRequest) async throws -> (Data, URLResponse)
+}
+
+extension URLSession: URLSessionProtocol {
+    public func data(for request: URLRequest, delegate: URLSessionTaskDelegate?) async throws -> (Data, URLResponse) {
+        try await data(for: request)
+    }
+}
+
+class URLSessionHTTPClient: HTTPClient {
+    private let session: URLSessionProtocol
+        
+    init(session: URLSessionProtocol = URLSession.shared) {
+        self.session = session
+    }
+    
+    func get(from url: URL) async throws -> HTTPResponse {
+        let urlRequest = URLRequest(url: url)
+        let (data, urlResponse) = try await session.data(for: urlRequest)
+               
+        return (data, urlResponse as! HTTPURLResponse)
+    }
+}
+
+public struct UnsupportedURLResponseError: Error {}
+
+final class URLSessionHTTPClientTests: XCTestCase {
+    
+    func test_getFromURL_performsGETRequestWithURL() async throws {
+        let (sut, session) = makeSUT()
+        
+        _ = try await sut.get(from: anyURL())
+        
+        XCTAssertNotNil(session.request)
+    }
+    
+
+    // MARK: - Helpers
+    
+    private func makeSUT(
+        result: Result<(Data, URLResponse), Error> = .success((Data(), HTTPURLResponse()))
+    ) -> (sut: URLSessionHTTPClient, session: URLSessionProtocolStub) {
+        let sessionStub = URLSessionProtocolStub(result: result)
+        let sut = URLSessionHTTPClient(session: sessionStub)
+
+        return (sut, sessionStub)
+    }
+    
+    private func anyURL() -> URL {
+        URL(string: "http://any-url.com")!
+    }
+    
+    private func anyValidResponse() -> (Data, HTTPURLResponse) {
+        (Data(), anyValidResponse())
+    }
+    
+    private func anyValidResponse() -> HTTPURLResponse {
+        httpResponse(code: 200)
+    }
+    
+    private func httpResponse(code: Int) -> HTTPURLResponse {
+        HTTPURLResponse(url: URL(string: "http://any-url.com")!, statusCode: code, httpVersion: nil, headerFields: nil)!
+    }
+}
+
+private class URLSessionProtocolStub: URLSessionProtocol {
+    private let result: Result<(Data, URLResponse), Error>
+    var request: URLRequest?
+    
+    init(result: Result<(Data, URLResponse), Error>) {
+        self.result = result
+    }
+    
+    func data(for request: URLRequest) async throws -> (Data, URLResponse) {
+        self.request = request
+        return try result.get()
+    }
+}
